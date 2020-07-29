4a31037704f4caef7b955f58f33669405154f015
Alon Gal <alon@stampme.com>
2020-07-29 19:48:55 +1000

    added docker and installed laravel

---
+++ docker/install-create-project.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm artisan;
+
+if [ -z "$(ls -A /var/www/html/src)" ]; then
+    # install laravel new project
+    composer create-project laravel/laravel .
+else
+   # do nowthing
+   echo ""
+fi
